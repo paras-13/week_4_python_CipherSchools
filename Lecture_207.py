c8f327b1eb5f3fab6c9f31dce82d881e649281d6
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-01-06 15:02:26 +0530

    NotImplementedError in python

---
+++ Lecture_207.py
@@ -0,0 +1,28 @@
+# raise error example 1
+# NonImplementedError
+# abstract method
+
+class Animal:
+    def __init__(self,name):
+        self.name = name
+    def sound(self):
+        raise NotImplementedError("you have to define this method in subclass")
+    
+class Dog(Animal):
+    def __init__(self,name,breed):
+          super().__init__(name)
+          self.breed = breed
+
+    def sound(self):
+        return "bhow bhow"
+
+class Cat(Animal):
+    def __init__(self,name,breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def sound(self):
+        return "meao meao"
+
+doggy = Dog("boony","pug")
+print(doggy.sound())

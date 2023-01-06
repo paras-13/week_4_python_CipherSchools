16eb8e117398c11089057205fca25fd5d31325e1
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-01-06 15:01:46 +0530

    Different types of error

---
+++ Lecture_205.py
@@ -0,0 +1,29 @@
+# built in error
+# exception, how to handle them
+# raise your own errors, debugging, etx.
+
+# Syntax Error
+# def func:
+#     pass
+
+# Indentation error
+# def func():
+#     print("hello")
+#   print("world")
+
+# Index Error
+# l = [1,2,3]
+# print(l[4])
+
+# Value Error
+# s = "abc"
+# print(int(s))
+
+# Attribute error
+
+# l = [1,2,3]
+# l.push("12")
+
+# Key error
+# d = {"name":"paras"}
+# print(d["age"])
